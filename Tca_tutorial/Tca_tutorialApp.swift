4a27dc69e144d42c94a8c33bbde9dc08d861f43e
ymurao <ymurao@yahoo-corp.jp>
2023-01-01 10:02:17 +0900

    first commit

---
+++ Tca_tutorial/Tca_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tca_tutorialApp.swift
+//  Tca_tutorial
+//
+//  Created by ymurao on 2023/01/01.
+//
+
+import SwiftUI
+
+@main
+struct Tca_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
